1ab5c98aa6fb18e042c044eb3b7b131bd94b749c
Guillaume Gaisne <guillaume.gaisne@esrf.fr>
2025-05-20 15:02:42 +0200

    Cruft update

--- move_template_files.sh
+++ move_template_files.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-rsync -avh nomad-SXDM_parser/ .
-rm -rfv nomad-SXDM_parser
+if ! command -v rsync >/dev/null 2>&1; then
+  echo "rsync required, but not installed!"
+  exit 1
+else
+  rsync -avh nomad-SXDM_parser/ .
+  rm -rfv nomad-SXDM_parser
+fi
